2d128300e99569d1803f68587425f94ad4c2f0d7
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-04-18 13:45:43 +0600

    add to navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Headers from '../Headers/Headers';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
+            <Headers></Headers>
             <Banner></Banner>
             <Services></Services>
         </div>
