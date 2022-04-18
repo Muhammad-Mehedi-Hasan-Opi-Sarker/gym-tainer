a48dc50c61f5631478e35fdf3057c0df71e7c97e
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-04-18 18:11:07 +0600

    add to email login and password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 const Login = () => {
     const navigate = useNavigate();
+    const emailRef =useRef('');
+    const passwordRef = useRef('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+      if(user){
+        navigate('/home');
+    }
     const handleLogin =event=>{
         event.preventDefault();
-        // console.log(event.target.email.value)
+        const email=emailRef.current.value;
+        const password =passwordRef.current.value;
+        signInWithEmailAndPassword(email,password);
     }
+   
     return (
         <div className='login'>
             <h1 style={{color:'rgba(243, 111, 33, 0.86)'}}>Login</h1>
             <form onSubmit={handleLogin}>
                 <label htmlFor="email">Email:</label>
-                <input type="email" name="email" id="" placeholder='Enter you email' required /><br />
+                <input ref={emailRef} type="email" name="email" id="" placeholder='Enter you email' required /><br />
                 <label htmlFor="password">Password:</label>
-                <input type="password" name="password" id="" required placeholder='Password' /><br />
+                <input ref={passwordRef} type="password" name="password" id="" required placeholder='Password' /><br />
                 <p>No have a account <small><Link className='text-decoration-none' to={'/register'} >Please Register</Link></small></p>
                 <input style={{backgroundColor:'rgba(243, 111, 33, 0.86)'}} className='btn' type="submit" value="submit" />
             </form>
