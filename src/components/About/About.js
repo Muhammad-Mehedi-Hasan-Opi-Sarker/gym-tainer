d5438bb061294ba6cb453abf5d4232a535f753c9
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-04-18 23:19:22 +0600

    add to blog and about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './About.css';
+import opi from '../../images/picture/opi.jpg'
 const About = () => {
     return (
-        <div className='about'>
-            <h1>About</h1>
-            <p>dfslk</p>
-        </div>
+        <div className='about container'>
+            
+            
+                <div>
+                <img className='w-50' src={opi} alt="" />
+                </div>
+                <div>
+                    <h4 className='text-danger'>My Goal</h4>
+                    <p>My goal is to give something to Bangladesh. There is a desire to do more for this country.
+                         First I will eradicate poverty from this country inshallah. I will try with other third world countries. 
+                        I will sacrifice my life to protect Al-Aqsa Mosque from the Israeli occupation, God willing.</p>
+                </div>
+            </div>
+       
     );
 };
 
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,60 @@
 import React from 'react';
-
+import './Blog.css';
 const Blog = () => {
     return (
-        <div>
+        <div className='blog container'>
             <h1>Blog</h1>
+            <h1>No 1 : Difference between authorization and authentication</h1>
+            <p>Ans :</p>
+            <div className='d-flex'>
+                <div>
+                    <h5>Authentication</h5>
+                    <ul>
+                        <li>Authentication verifies who the user is.</li>
+                        <li>Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.</li>
+                        <li>Authentication is the first step of a good identity and access management process.</li>
+                        <li>Authentication is visible to and partially changeable by the user.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Authorization</h5>
+                    <li>Authorization determines what resources a user can access.
+                    </li>
+                    <li>Authorization works through settings that are implemented and maintained by the organization.</li>
+                    <li>Authorization always takes place after authentication.</li>
+                    <li>Authorization isn’t visible to or changeable by the user.</li>
+                </div>
+            </div>
+            <div>
+                <h1>No 2: Why are you using firebase? What other options do you have to implement authentication?</h1>
+                <p>Ans :</p>
+                <div>
+                    <p>Firebase is a lot of method. It can be used to verify email password.
+                        These tasks are made much easier with the help of Firebase.
+                        Firebase makes it easy to login and sign in.</p>
+                    <p>1. Back4App</p>
+                    <p>2. AWS Amplify</p>
+                    <p>3. Kuzzle</p>
+                    <p>4. Couchbase</p>
+                    <p>5. NativeScript</p>
+                    <p>6. Flutter</p>
+
+                </div>
+            </div>
+            <div>
+                <h1>NO 3: What other services does firebase provide other than authentication?</h1>
+                <p>Ans :</p>
+                <div>
+                    <p>1. Cloud Firestore.</p>
+                    <p>2. Cloud Functions.</p>
+                    <p>3. Authentication.</p>
+                    <p>4. Hosting.</p>
+                    <p>5. Cloud Storage.</p>
+                    <p>6. Google Analytics.</p>
+                    <p>7. Predictions.</p>
+                    <p>8. Cloud Messaging.</p>
+                </div>
+            </div>
         </div>
     );
 };
